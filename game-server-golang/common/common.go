0a2238b8513cf9a2a2a2f05cd708b35a7dcfeaa3
simpleyo <energy2011@gmail.com>
2024-07-09 10:19:09 +0200

    Add files via upload

---
+++ game-server-golang/common/common.go
@@ -0,0 +1,21 @@
+package common
+
+// type IDR struct {
+// 	value int
+// }
+
+// func NoneIDR() IDR {
+// 	return IDR{0}
+// }
+
+// func NewIDR(x int) IDR {
+// 	return IDR{x + 1}
+// }
+
+// func (idr IDR) IsValid() bool {
+// 	return idr.value > 0
+// }
+
+// func (idr IDR) V() int {
+// 	return idr.value - 1
+// }
