63ce76e708c240885820effad2299368329d6bb5
simpleyo <energy2011@gmail.com>
2024-07-09 10:31:30 +0200

    Add files via upload

---
+++ game-server/tools/do-backup.bat
@@ -0,0 +1,2 @@
+robocopy C:\DEV\Projects\rigel E:\DEV\Backup\rigel\rigel /E /XO /XF "*.obj" "*.lib" "*.exe" "*.pdb" "*.idb" "*.suo" "*.db" "*.ilk" "*.iobj" "*.ipdb" "*.user" "*.exp" "*.wast" "*.wasm" "*.pyc" "*.bc" "*.opendb" "*.ipch"
+
---
+++ game-server/tools/launch-account-server.bat
@@ -0,0 +1,5 @@
+start /d "E:\Program Files\MongoDB\Server\4.0\bin\" mongod.exe
+start /d "C:\Program Files\nodejs\" node.exe C:\DEV\Projects\gameland\database-rpc-server\app.js
+start /d "C:\DEV\Projects\gameland\account-server" E:\DEV\Tools\Python\Python36\python.exe account_server.py
+
+
